47662bf3a9e6913fa06c8ddecd3d9cf468f912ff
Seb <sebtoombs@gmail.com>
2021-01-10 09:46:46 +1100

    Update verifyHash.js

--- packages/next-iron-auth/src/lib/verifyHash.js
+++ packages/next-iron-auth/src/lib/verifyHash.js
@@ -1,5 +1,9 @@
 import argon2 from "argon2";
 
 export default async function verifyHash(hash, password) {
+  // Make sure hash actually exists
+  if (!hash || !password) {
+    throw new Error("Hash or password empty");
+  }
   return argon2.verify(hash, password);
 }
